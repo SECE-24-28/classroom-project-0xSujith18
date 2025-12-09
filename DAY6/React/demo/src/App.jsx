083603e295e3623cd533f513397a80f7628d8bba
0xSujith18 <sujithshankar18@gmail.com>
2025-12-09 14:11:12 +0530

    commit

--- DAY6/React/demo/src/App.jsx
+++ DAY6/React/demo/src/App.jsx
@@ -9,8 +9,10 @@ function App() {
 
   return (
     <>
-      <p>I am from {city}</p>
-      <p></p>
+      <div className="margin-box">
+        <h1>Welcome to {city}!</h1>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt consectetur omnis odit illo est nobis voluptate consequatur delectus assumenda, tenetur sit hic eligendi veritatis libero itaque dolore doloremque repudiandae error. Quod, ipsa quis, consequatur animi alias sapiente nulla rerum tenetur odio libero in excepturi quibusdam repellat fuga! Maxime, magni distinctio!</p>
     </>
   )
 }
