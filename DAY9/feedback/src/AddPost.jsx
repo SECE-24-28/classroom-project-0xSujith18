1b29ae5487d1c25fc4dedc05136d519e8ea97cec
0xSujith18 <sujithshankar18@gmail.com>
2025-12-11 19:38:38 +0530

    commit

---
+++ DAY9/feedback/src/AddPost.jsx
@@ -0,0 +1,13 @@
+const AddPost = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="text" placeholder='Title' />
+            <input type="text" placeholder='content' />
+            <button type="submit">Add Post</button>
+        </form>
+    </div>
+  )
+}
+
+export default AddPost
--- DAY9/feedback/src/App.jsx
+++ DAY9/feedback/src/App.jsx
@@ -3,11 +3,14 @@ import './App.css'
 import api from './api/Post'
 import Home from './Home.jsx'
 import Search from './Search'
+import AddPost from './AddPost.jsx'
 
 function App() {
   const [post, setpost] = useState([])
   const [search, setsearch] = useState('')
   const[searchResults,setSearchResults]=useState([])
+  const[title,settitle]=useState('')
+  const[content,setcontent]=useState('')
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -16,11 +19,7 @@ function App() {
         setpost(res.data || [])
       } catch (error) {
         console.error('Error fetching posts:', error)
-        // Try alternative endpoints or use mock data
-        setpost([
-          { id: 1, title: 'Sample Post 1', content: 'This is sample content 1' },
-          { id: 2, title: 'Sample Post 2', content: 'This is sample content 2' }
-        ])
+        setpost([])
       }
     }
     fetchPosts();
@@ -40,6 +39,8 @@ return(
 
 <Search search={search} setsearch={setsearch}
   />
+  <hr/>
+  <AddPost/>
     <Home posts={searchResults} />
   </>
 )
